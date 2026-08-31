d82db76ef0f0ad5b9e6bf4b792e304b0a2495963
CATIN--RICO Charly <charly44310@gmail.com>
2026-08-31 17:21:14 +0200

    test debug

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // le premier qui se connecte sera la joueur 1
 
 // serveur debug
-const socket = io("[http://VOTRE_IP_PROXMOX:3000](http://192.168.1.50:3000)");
+const socket = io("[http://192.168.1.50:3000](http://192.168.1.50:3000)");
 
 
 class Carte {
